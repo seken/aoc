927f7da1a6a949feb8cc0d1f8f77836216668de4
Mark Goodall <markit@seken.co.uk>
2016-12-06 21:12:02 +0000

    Day 6 done

---
+++ 2016/day6/part1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import hashlib
+
+codes = sys.stdin.read().split('\n')
+
+hist = [dict() for i in range(len(codes[0]))]
+
+for code in codes:
+    for pos, letter in enumerate(code):
+        if letter in hist[pos]:
+            hist[pos][letter] += 1
+        else:
+            hist[pos][letter] = 1
+
+code = ''
+
+for h in hist:
+    code += sorted(list(h.iteritems()), key=lambda x : x[1], reverse=True)[0][0]
+
+print(code)
